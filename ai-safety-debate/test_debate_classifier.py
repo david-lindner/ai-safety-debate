e4921eb6e9dd8c8e2517e92c9c18df48d7f7c7cf
David Lindner <dev@davidlindner.me>
2019-05-02 11:49:32 +0200

    add loss weights

--- ai-safety-debate/test_debate_classifier.py
+++ ai-safety-debate/test_debate_classifier.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from judge import MNISTJudge
 from agent import DebateClassifier
 
@@ -15,6 +17,7 @@ if __name__ == "__main__":
         batch_end = min(batch_start + 128, len(train_data))
         batch = train_data[batch_start:batch_end]
         labels = train_labels[batch_start:batch_end]
-        debate_classifier.train(batch, labels)
+        loss_weights = np.ones_like(labels, dtype=np.float32)
+        debate_classifier.train(batch, labels, loss_weights)
     acc = debate_classifier.evaluate_accuracy(eval_data, eval_labels)
     print("Accuracy", acc)

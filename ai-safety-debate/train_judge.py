4621130c0c59f7d0b5f2f8b68610e3731b906c2e
David Lindner <dev@davidlindner.me>
2019-05-02 12:55:18 +0200

    fix saving judge

--- ai-safety-debate/train_judge.py
+++ ai-safety-debate/train_judge.py
@@ -3,7 +3,9 @@ import argparse
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--dataset", type=str, help="Currently only 'mnist' or 'fashion'")
+    parser.add_argument(
+        "--dataset", type=str, help="Currently only 'mnist' or 'fashion'"
+    )
     parser.add_argument(
         "--N-features", type=int, help="Number of features revealed as an input"
     )
@@ -14,9 +16,9 @@ if __name__ == "__main__":
     path = args.path or "saved_models/" + args.dataset + str(args.N_features)
 
     if args.dataset == "mnist":
-        judge = MNISTJudge(N_to_mask=args.N_features, save_model_as=path)
+        judge = MNISTJudge(N_to_mask=args.N_features, model_dir=path)
     elif args.dataset == "fashion":
-        judge = FashionJudge(N_to_mask=args.N_features, save_model_as=path)
+        judge = FashionJudge(N_to_mask=args.N_features, model_dir=path)
     else:
         raise Exception("Unknown dataset " + args.dataset)
 

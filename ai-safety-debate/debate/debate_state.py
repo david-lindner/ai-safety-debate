7414c52c231c32c8c2f345e4055cc1e966647a8d
vojta <vojta.kovarik@seznam.cz>
2019-05-03 22:58:25 +0200

    made wannabe-documentation for stuff that I knew how it works

--- ai-safety-debate/debate/debate_state.py
+++ ai-safety-debate/debate/debate_state.py
@@ -3,16 +3,18 @@ import copy
 
 
 class DebateState:
+    """
+    Keeps track of the "ground truth", the claims have made so far, the judge (for evaluation of the final result).
+    """
     def __init__(
         self,
         sample,
         initial_statements,
         judge,
-        moves_left=6,
-        starting_player=0,
-        player_order=None,
+        player_order,   # a vector of player labels -- who acts in which turn
+        moves_left=6,   # debate has to tell the state how many moves can we make
     ):
-        # debate has to tell the state how many moves can we make
+
         self.sample = sample
         self.mask = np.stack((np.zeros_like(sample), np.zeros_like(sample)))
         self.initial_statements = initial_statements
@@ -23,21 +25,23 @@ class DebateState:
 
         # player order is an array determinating which player should play each round
         # starting_player is not used anymore
-        self.currentPlayer = player_order[0]
+        self.current_player = player_order[0]
         self.player_order = player_order
 
     def getPossibleActions(self):
-        # not selected and nonzero feature
+        """Shows the not selected and nonzero features."""
         return np.where((self.mask.sum(axis=0) == 0) & (self.sample != 0))[0]
 
     def takeAction(self, action):
+        """Adds the latest action to the mask and changes the player."""
         assert action in self.getPossibleActions()
         newState = copy.copy(self)
         newState.mask = np.copy(self.mask)
-        newState.mask[newState.currentPlayer, action] = 1
+        newState.mask[newState.current_player, action] = 1
         newState.moves_left -= 1
+        # to prevent potential unexpected behavior in terminal states, we explicitly set the player to None there
         if newState.moves_left == 0:
-            newState.currentPlayer = None
+            newState.current_player = None
         else:
             moves_past = len(newState.player_order) - newState.moves_left
             if (
@@ -49,21 +53,25 @@ class DebateState:
         return newState
 
     def maximizerNode(self):
+        """This tells MCTS whether it should try to optimize from first players POV or from the second players POV."""
         # return +1 if the current player is a maximizer, -1 if they are a minimizer
-        if self.currentPlayer == 0:
+        if self.current_player == 0:
             return 1
-        elif self.currentPlayer == 1:
+        elif self.current_player == 1:
             return -1
         else:
             return None
 
     def isTerminal(self):
+        """This keeps track of whether the game has ended. Important for MCTS and for knowing when to get rewards."""
         assert self.moves_left >= 0
         return self.moves_left == 0
 
     def getReward(self):
+        """Calls the judge (atm a neural network, but possibly more general thingy) to compute utility to pl. 1."""
         assert self.isTerminal()
         mask = self.mask.sum(axis=0)
+        # the judge needs a "shape" of the mask (which pixels got revealed) and the values of those pixels.
         utility = self.judge.evaluate_debate(
             np.stack((mask, self.sample * mask), axis=1), self.initial_statements
         )
